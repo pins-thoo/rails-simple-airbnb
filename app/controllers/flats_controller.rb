841034043919432e62541ae83233b033caf18309
Pins Thoo <aipeng.thoo@gmail.com>
2020-05-13 16:20:47 +0800

    controller half done

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,37 @@
+class FlatsController < ApplicationController
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating flats..."
+
+4.times do
+  flat = Flat.new(
+    name: Faker::Commerce.department(max: 5),
+    address: Faker::Address.unique.street_address,
+    description: Faker::Company.catch_phrase,
+    price_per_night: Faker::Number.non_zero_digit,
+    number_of_guests: Faker::Number.non_zero_digit
+  )
+  flat.save!
+end
+
+puts "Finish creating flats"
